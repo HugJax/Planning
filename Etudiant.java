2101d068cf8f59c04610b2c6098f0706a7615e54
rf176131 <47567983+rf176131@users.noreply.github.com>
2020-05-17 12:46:00 +0200

    Première version bases du contrôleur
    il faut qu'on se mette d'accord sur les types de variables à utiliser

---
+++ Etudiant.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+/**
+ *
+ * @author Rémi Fiorenza
+ */
+public class Etudiant extends Utilisateur
+{
+    //ATTRIBUTS
+    private int m_numero;
+    private Groupe m_groupe;
+    
+    //METHODES
+    
+}
