2641d325f38fb45bedefb0202305b215f775ea63
rf176131 <47567983+rf176131@users.noreply.github.com>
2020-06-04 20:01:36 +0200

    version affichage final

--- AfficheProfil.java
+++ AfficheProfil.java
@@ -11,6 +11,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import Modele.*;
 import Controleur.*;
+import java.awt.GridLayout;
+import java.awt.Label;
+import javax.swing.JTable;
 
 /**
  *
@@ -31,31 +34,64 @@ public class AfficheProfil extends JPanel{
     
     /// CONSTRUCTEURS
     public AfficheProfil(int iduser){
+        
+        
         useractif = con.retrieveUserFromID(iduser);
-        String info = "<html><ul><li>Nom :" + useractif.getNom() + "</li><li>Prénom :" + useractif.getPrenom();
+        String info = ""; //= "<html><ul><li>Nom :" + useractif.getNom() + "</li><li>Prénom :" + useractif.getPrenom();
+        String infoVa = null;
         if(useractif.getDroit()==4)
         {
             etu = con.retrieveEtudiantFromID(iduser);
             grp = conbis.retrieveGroupe(etu.getGroupe());
             prom = conbis.retrievePromo(etu.getPromo());
             info = info + "</li><li>Promo :" + prom.getPromo() + "</li><li>Groupe :" + grp.getGroupe();
+            infoVa = "Promo et Groupe :";
         }
         if(useractif.getDroit()==3)
         {
             prof = con.retrieveEnseignantFromID(iduser);
             cours = conbis.retrieveCours(prof.getIDcours());
-            info = info + "</li><li>Matière :" + cours.getNom();
+            info = cours.getNom();
+            //info = info + "</li><li>Matière :" + cours.getNom();
+            infoVa = "Matière :";
         }
             
-        info = info + "</li></ul></html>";
-        profil = new JLabel(info);
+        //info = info + "</li></ul></html>";
+        
+
+        
+        
+        add(new JLabel("<html><h2> Nom :</h2></html>", null, JLabel.CENTER));
+        add(new JLabel("<html><h2> Prenom :</h2></html>", null, JLabel.CENTER));
+        add(new JLabel("<html><h2>" + infoVa + "</h2></html>", null, JLabel.CENTER));
+        
+        String temp="<html>"  + useractif.getNom() + "</html>";
+        JLabel j1 = new JLabel(temp, null, JLabel.CENTER);
+        
+        String temp1="<html>"  + useractif.getPrenom()+ "</html>";
+        JLabel j2 = new JLabel(temp1, null, JLabel.CENTER);
+        
+        JLabel j3 = new JLabel(info, null, JLabel.CENTER);
+        
+        JLabel j4 = new JLabel("", null, JLabel.CENTER);
+        
+        
+       
+        
+      //  profil = new JLabel(info);
         bouton = new JButton("Déconnexion");
         bouton.setBackground(Color.RED);
         bouton.addActionListener(new Deco());
-        this.add(profil);
+        add(j1);
+        add(j2);
+        add(j3);
+        add(j4);
+      //  this.add(profil);
         this.add(bouton);
         this.setVisible(true);
         this.validate();
+        
+        setLayout(new GridLayout(3,2));
     }
     
     /// METHODES
